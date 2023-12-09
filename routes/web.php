3359dd73d612a40f9c54f87f9d50ae263159da74
Sérgio Ribeiro <sergio.m.rib@outlook.com>
2023-12-09 17:53:49 +0000

    Latest version: Graph working

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [InertiaDemoController::class, 'singleChart']);
 Route::get('/region', [InertiaDemoController::class, 'multipleChart']);
+Route::get('/sayhi', function() { 
+    return "Hi!";
+});
