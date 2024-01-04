aefef168d77a9154315020bb218b79efa6c5fc2d
Sérgio Ribeiro <sergio.m.rib@outlook.com>
2024-01-04 02:23:05 +0000

    Added survey page

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class SurveyController extends Controller
 {
     public function index() {
 
-        return Question::all();
+        return Inertia::render('Survey', [
+            'questions' => Question::all()
+        ]);
 
     }
 }
---
+++ resources/js/Pages/Survey.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { inertia, page } from '@inertiajs/inertia-svelte'
+
+  export let questions = []
+
+  //$title = 'Questions'
+
+</script>
+
+<h1 class="mb-8 font-bold text-3xl">Survey</h1>
+
+<div class="bg-white rounded-md shadow overflow-x-auto">
+
+    {#each questions as question (question.id)}
+        <p class="mx-4">{ question.title}</p>
+    {/each}
+
+    {#if questions.length === 0}
+        <p class="border-t px-6 py-4" colspan="4">No questions found.</p>
+    {/if}
+</div>
